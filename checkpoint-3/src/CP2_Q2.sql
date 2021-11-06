1e7655dd0233f71f1cc943bf48a441c018dc1502
bodhisattamaitinorthwestern <91136792+bodhisattamaitinorthwestern@users.noreply.github.com>
2021-11-06 14:39:12 -0500

    cp3 q2 version 5

--- checkpoint-3/src/CP2_Q2.sql
+++ checkpoint-3/src/CP2_Q2.sql
@@ -37,16 +37,51 @@ SELECT
            ', AC:', DOF.allegation_count,
            ', DC:', DOF.discipline_count,
            ', HMC:', DOF.honorable_mention_count
-           ) AS FULL_DETAILS_OFFICER
+           ) AS full_details_officer
 FROM
        tfd_cp3_officer_disciplined_allegation ODA
 INNER JOIN data_officer DOF on ODA.officer_id = DOF.id;
 
+-- testing
 SELECT * FROM tfd_cp3_officer_details_disciplined_allegation LIMIT 20;
 
+DROP VIEW IF EXISTS tfd_cp3_total_officers_disciplined_per_complaint;
+CREATE VIEW tfd_cp3_total_officers_disciplined_per_complaint AS
 SELECT
       crid,
-      COUNT(*) num_of_officers_disciplined_in_complaint
+      COUNT(*) num_of_officers_disciplined_per_complaint
 FROM tfd_cp3_officer_disciplined_allegation
 GROUP BY crid
-ORDER BY num_of_officers_disciplined_in_complaint DESC;
+ORDER BY num_of_officers_disciplined_per_complaint DESC;
+
+-- testing
+SELECT * FROM tfd_cp3_total_officers_disciplined_per_complaint LIMIT 10;
+
+DROP VIEW IF EXISTS tfd_cp3_officer_disciplined_final_details;
+CREATE VIEW tfd_cp3_officer_disciplined_final_details AS
+SELECT
+      ODDA.crid,
+      ODDA.incident_date,
+      ODDA.incident_year,
+      ODDA.officer_id,
+      ODDA.disciplined,
+      ODDA.gender,
+      ODDA.race,
+      ODDA.rank,
+      ODDA.first_name,
+      ODDA.last_name,
+      ODDA.allegation_count,
+      ODDA.discipline_count,
+      ODDA.honorable_mention_count,
+      ODDA.full_details_officer,
+      ODPC.num_of_officers_disciplined_per_complaint
+FROM tfd_cp3_officer_details_disciplined_allegation ODDA
+INNER JOIN tfd_cp3_total_officers_disciplined_per_complaint ODPC on ODDA.crid = ODPC.crid;
+
+-- testing
+SELECT * FROM tfd_cp3_officer_disciplined_final_details LIMIT 10;
+
+-- for this question, we are primarily interested in the scenario where multiple officers have been disciplined together in a complaint
+SELECT * FROM tfd_cp3_officer_disciplined_final_details
+WHERE num_of_officers_disciplined_per_complaint > 1
+ORDER BY crid;
