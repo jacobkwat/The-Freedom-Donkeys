abd3055761c75eefc89408c15fadff8f236d61ce
bodhisattamaitinorthwestern <91136792+bodhisattamaitinorthwestern@users.noreply.github.com>
2021-11-28 23:18:49 -0600

    sql query version 2

--- checkpoint-5/src/CP5_Qs.sql
+++ checkpoint-5/src/CP5_Qs.sql
@@ -11,6 +11,8 @@ FROM
        data_attachmentfile ATF
 INNER JOIN data_attachmentnarrative ATN on ATF.id = ATN.attachment_id;
 
+SELECT COUNT(*) FROM tfd_cp5_complaint_description_details;
+
 DROP VIEW IF EXISTS tfd_cp5_allegation_and_category_details;
 CREATE TEMP VIEW tfd_cp5_allegation_and_category_details AS
 SELECT
@@ -25,6 +27,8 @@ SELECT
 FROM
        data_allegation DA
 INNER JOIN data_allegationcategory DAC on DA.most_common_category_id = DAC.id;
+
+SELECT COUNT(*) FROM tfd_cp5_allegation_and_category_details;
 
 DROP VIEW IF EXISTS tfd_cp5_allegation_complaint_description_details;
 CREATE TEMP VIEW tfd_cp5_allegation_complaint_description_details AS
@@ -41,5 +45,54 @@ FROM
        tfd_cp5_allegation_and_category_details ACD
 INNER JOIN tfd_cp5_complaint_description_details CDD on ACD.crid = CDD.allegation_id;
 
+SELECT COUNT(*) FROM tfd_cp5_allegation_complaint_description_details;
+
+DROP VIEW IF EXISTS tfd_cp5_officer_allegation_details;
+CREATE TEMP VIEW tfd_cp5_officer_allegation_details AS
+SELECT
+       ACDD.crid,
+       ACDD.incident_year,
+       ACDD.is_officer_complaint,
+       ACDD.cr_text,
+       ACDD.category,
+       ACDD.allegation_name,
+       ACDD.text_content,
+       ACDD.complaint_narrative,
+       OA.officer_id,
+       OA.disciplined
+
+FROM
+       tfd_cp5_allegation_complaint_description_details ACDD
+INNER JOIN data_officerallegation OA on ACDD.crid = OA.allegation_id;
+
+DROP VIEW IF EXISTS tfd_cp5_officer_allegation_description_complete_details;
+CREATE TEMP VIEW tfd_cp5_officer_allegation_description_complete_details AS
+SELECT
+       OAD.crid,
+       OAD.incident_year,
+       OAD.is_officer_complaint,
+       OAD.cr_text,
+       OAD.category,
+       OAD.allegation_name,
+       OAD.text_content,
+       OAD.complaint_narrative,
+       OAD.officer_id,
+       OAD.disciplined,
+       DOF.gender,
+       DOF.race,
+       DOF.rank,
+       DOF.current_salary,
+       DOF.allegation_count,
+       DOF.discipline_count,
+       DOF.honorable_mention_count,
+       DOF.major_award_count,
+       DOF.civilian_compliment_count
+FROM
+       tfd_cp5_officer_allegation_details OAD
+INNER JOIN data_officer DOF on OAD.officer_id = DOF.id;
+
+-- extract csv after executing the below lines:
+SELECT * FROM tfd_cp5_officer_allegation_description_complete_details;
+SELECT COUNT(*) FROM tfd_cp5_officer_allegation_description_complete_details WHERE disciplined=TRUE;
 
 
